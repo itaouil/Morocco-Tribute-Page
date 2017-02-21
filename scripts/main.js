9a84ddaa6d35a853202126eca223d573c85044de
ilyass <ilyass.taouil@caterva.de>
2017-02-21 09:54:29 +0100

    Refactoring in process.

--- scripts/main.js
+++ scripts/main.js
@@ -1,66 +1,53 @@
 //When window is loaded (images included)
 $( window ).on( "load", function() {
-	
+
 	/* Google Map map */
 	initMap();
-	
+
 	function initMap() {
-		
+
 		var morocco = {lat: 31.669746, lng: -7.973328};
-		var moroccoAsWell = {lat: 27.125286, lng: -13.162500};
-		
-    var map = new google.maps.Map(document.getElementById('googleMap'), {
+
+		var map = new google.maps.Map(document.getElementById('googleMap'), {
 			zoom: 5,
 			center: morocco
 		});
-        
+
 		var marker = new google.maps.Marker({
 			position: morocco,
-      map: map
-    });
-		
-		var marker = new google.maps.Marker({
-			position: moroccoAsWell,
-      map: map
-    });
-		
+			map: map
+		});
+
 	}
-	
+
 	/* Sticky Header on scroll */
 	var stickyNavTop = $('nav').offset().top;
-	var stickyNavBottome = $('nav').offset().bottom;
-	
-	/* Toggle Nav Section */
-	var destinationsNavTop = $('#destinations-section');
-	
-	/* Toggle Nav Section */
-	var mapNavTop = $('#map-section');
-	
+
 	//Define Function
 	var stickyNav = function(){
-		
+
 		var scrollTop = $(window).scrollTop();
-      
-		if (scrollTop > stickyNavTop) { 
+
+		if (scrollTop > stickyNavTop) {
 			$('nav').addClass('sticky');
 			$('#glimpse-nav').addClass('nav-toggle');
-		} 
-		
+		}
+
 		else {
-    	$('nav').removeClass('sticky'); 
+			$('nav').removeClass('sticky');
 			$('#glimpse-nav').removeClass('nav-toggle');
 		}
-		
+
 	};
- 
+
 	stickyNav();
- 
+
 	$(window).scroll(function() {
-  	stickyNav();
+		stickyNav();
 	});
-	
-	
-	
-	
-	
+
+
+
+
+
 });
